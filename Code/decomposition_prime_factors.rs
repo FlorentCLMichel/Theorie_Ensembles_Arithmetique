4054e70eca393a24401934e4b1040d44b6478d2e
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2022-04-09 01:52:03 +0100

    typos corrected

---
+++ Code/decomposition_prime_factors.rs
@@ -0,0 +1,77 @@
+#[path="./Érastosthène.rs"]
+#[allow(non_snake_case)]
+mod Érastosthène;
+use Érastosthène::primes_below_n;
+
+use std::io::Write;
+
+const N: usize = 213;
+
+fn decomposition_prime_factors(mut n: usize, primes: &[usize]) -> Result<Vec<(usize, usize)>, NotEnoughPrimesError> {
+    
+    // vector to store the result
+    let mut res = Vec::<(usize, usize)>::new();
+    
+    let mut index_prime = 0;
+    while n > 1 {
+        
+        if index_prime >= primes.len() {
+            return Err(NotEnoughPrimesError)
+        }
+        
+        let mut power: usize = 0;
+        while n % primes[index_prime] == 0 {
+            power += 1;
+            n /= primes[index_prime];
+        }
+
+        if power > 0 {
+            res.push((primes[index_prime], power));
+        }
+        
+        index_prime += 1;
+    } 
+
+    Ok(res)
+}
+
+fn print_decomposition(decomposition: &[(usize, usize)]) -> String {
+
+    let mut res = "".to_string();
+    for (p, a) in decomposition.iter() {
+        res += &format!("{}^{{{}}} × ", p, a);
+    }
+    let mut chars = res.chars();
+
+    // trim the last three characters
+    chars.next_back();
+    chars.next_back();
+    chars.next_back();
+    chars.as_str().to_string()
+}
+
+#[derive(Debug, Clone)]
+struct NotEnoughPrimesError;
+
+impl std::fmt::Display for NotEnoughPrimesError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Not enough prime numbers!")
+    }
+}
+
+impl std::error::Error for NotEnoughPrimesError {}
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let primes = primes_below_n(N);
+    let mut string_to_write = "".to_string();
+    for n in 2..=N {
+        string_to_write += &format!("${} = {}$\n\n", n, print_decomposition(&decomposition_prime_factors(n, &primes)?));
+    }
+    
+    let f_name = "output_decomposition_prime_factors.tex";
+    let mut output = std::fs::File::create(f_name)?;
+    write!(output, "{}", string_to_write)?;
+    
+    Ok(())
+}
