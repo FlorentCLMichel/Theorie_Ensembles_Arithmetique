c72d061b8de1593a2feaefd371fcdde9cc875f08
Florent Michel <florent.m42@gmail.com>
2025-09-24 23:20:58 +0100

    add C and Rust primality tests

--- Ensembles_Arithmetique/Code/is_prime.c
+++ Ensembles_Arithmetique/Code/is_prime.c
@@ -1,20 +1,11 @@
 bool is_prime(unsigned int n) {
-    
-    if (n < 2) {
-        return false;
-    } 
-    if (n < 4) {
-        return true;
-    }
-    if (n%2 == 0) {
-        return false;
-    }
+    if (n < 2) { return false; } 
+    if (n < 4) { return true; }
+    if (n % 2 == 0) { return false; }
 
     unsigned int m = 3;
-    while (m*m <= n) {
-        if (n%m == 0) {
-            return false;
-        }
+    while (m * m <= n) {
+        if (n % m == 0) { return false; }
         m += 2;
     }
 
--- Ensembles_Arithmetique/Code/is_prime.rs
+++ Ensembles_Arithmetique/Code/is_prime.rs
@@ -1,14 +1,7 @@
-fn is_prime(n: usize) -> bool{
-    
-    if n < 2 {
-        return false;
-    } 
-    if n < 4 {
-        return true;
-    }
-    if n%2 == 0 {
-        return false;
-    }
+fn is_prime(n: usize) -> bool {
+    if n < 2 { return false; } 
+    if n < 4 { return true; }
+    if n % 2 == 0 { return false; }
 
     let mut m: usize = 3;
     while m*m <= n {
